2e1cd755dbe99a0113fd7e516da4289226680202
Kaede Nakata <nakatakaede12345@gmail.com>
2022-02-26 18:35:55 +0900

    ランキングの実装完了

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
 'use strict';
+const fs = require('fs');
+const readline = require('readline');
+const re = fs.createReadStream('./popu-pref.csv');
+const rl = readline.createInterface({ input: re, output: {} });
+const perfectureDataMap = new Map(); // key：都道府県 value：集計データのオブジェクト
+rl.on('line', lineString => {
+    const columns = lineString.split(',');
+    const year = parseInt(columns[0]);
+    const perfecture = columns[1];
+    const popu = parseInt(columns[3]);
+    if (year === 2010 || year == 2015) {
+        let value = perfectureDataMap.get(perfecture);
+        if(!value){
+            value = {
+                popu10: 0,
+                popu15: 0,
+                change: null
+            };
+        }
+        if (year === 2010) {
+            value.popu10 = popu;
+        }
+        if (year === 2015) {
+            value.popu15 = popu;
+        }
+        perfectureDataMap.set(perfecture, value);
+    }
+});
+rl.on('close', () => {
+    for (let [key, value] of perfectureDataMap){
+        value.change = value.popu15 / value.popu10;
+    }
+    const rankingArray = Array.from(perfectureDataMap).sort((pair1,pair2) => {
+        return pair2[1].change -pair1[1].change;
+    });
+    const rankingStrings = rankingArray.map(([key, value]) => {
+        return (
+            key +
+            '：' +
+            value.popu10 +
+            '=>' +
+            value.popu15 +
+            '変化率：' +
+            value.change
+        );
+    });
+    console.log(rankingStrings);
+});
